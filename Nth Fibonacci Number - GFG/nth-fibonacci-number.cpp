173f57d22b7c5c1855e7eb17b7bb7d8152722f68
Aman Raj <90236749+amanraj5@users.noreply.github.com>
2023-08-07 19:31:49 +0530

    Added solution - LeetHub

---
+++ Nth Fibonacci Number - GFG/nth-fibonacci-number.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    int nthFibonacci(int n){
+        // code here
+        int N=1000000007;
+        if(n==0) return 0;
+        if(n==1) return 1;
+        long long int a=0,b=1,c;
+        for(int i=2;i<=n;i++){
+            c=(a+b)%N;
+            a=b;
+            b=c;
+        }
+        return c%N;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.nthFibonacci(n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
