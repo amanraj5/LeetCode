5f7f24f3c2ff768abaa4b85920436dfd8fc12e8f
Aman Raj <90236749+amanraj5@users.noreply.github.com>
2023-09-12 16:33:34 +0530

    Added solution - LeetHub

--- Transpose of Matrix - GFG/transpose-of-matrix.cpp
+++ Transpose of Matrix - GFG/transpose-of-matrix.cpp
@@ -12,7 +12,7 @@ class Solution
         // code here 
         for(int i=0;i<n;i++){
             
-            for(int j=i;j<n;j++){
+            for(int j=i+1;j<n;j++){
                 swap(matrix[i][j],matrix[j][i]);
             }
         }
